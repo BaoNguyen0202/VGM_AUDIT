c7ce454d2790477e0ea20b839f082ebabf2649cf
baonguyen <bao17030202@gmail.com>
2023-12-25 09:35:36 +0700

    api

--- src/const/api.const.ts
+++ src/const/api.const.ts
@@ -15,15 +15,17 @@ export const STT_INTERNAL_SERVER = 500;
 export const STT_NOT_MODIFIED = 304;
 const URL = 'http://10.0.0.67:8000';
 const URL_PREFIX = '/api/method/my_app';
-//POST
-export const POST_USER_LOGIN = URL + URL_PREFIX + '.auth.login';
+//POST_OLD
 export const POST_USER_LOGOUT = URL_PREFIX + '.auth.logout';
 export const POST_USER_ORGANIZATION = '/api/method/mbw_ess_registration.api.ess.organization.get_info_organization';
 export const POST_RESET_PASSWORD = URL_PREFIX + '.auth.reset_password';
-export const POST_SAVE_DOCS = URL + '/api/method/frappe.desk.form.save.savedocs';
-export const UPDATE_FILE_IMAGE = URL + '/api/method/upload_file';
+
 export const PUT_USER_PROFILE = URL_PREFIX + '.user.update_profile';
 
+//POST
+export const POST_USER_LOGIN = URL + URL_PREFIX + '.auth.login';
+export const POST_SAVE_DOCS = URL + '/api/method/frappe.desk.form.save.savedocs';
+export const UPDATE_FILE_IMAGE = URL + '/api/method/upload_file';
 //GET
 export const GET_USER_PROFILE = URL + URL_PREFIX + '.user.get_employee_info';
 export const GET_SCENARIO = URL_PREFIX + '/api/resource/Scenario';
